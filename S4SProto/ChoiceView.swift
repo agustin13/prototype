bafb601c3dbc84f2c9cdc6c4ccce61837c10df47
Sergio Perez <sergioperez@guest-wireless-upc-1606-10-120-096-125.usc.edu>
2016-07-19 11:49:41 -0700

    initial commit

---
+++ S4SProto/ChoiceView.swift
@@ -0,0 +1,20 @@
+//
+//  ChoiceView.swift
+//  S4SProto
+//
+//  Created by Sergio Perez on 7/15/16.
+//  Copyright © 2016 s4s. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChoiceView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ S4SProto/Login.swift
@@ -0,0 +1,19 @@
+//
+//  Login.swift
+//  S4SProto
+//
+//  Created by Sergio Perez on 7/15/16.
+//  Copyright © 2016 s4s. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
